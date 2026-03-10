e68da0179e801c3f8d20c1031f238ccfb6c0da31
karnVen <onemonth06@gmail.com>
2026-03-10 06:17:24 +0530

    feat: gameLoop completed selected delta logic for game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 //blue print ... we used JPanel and add our rule also 
@@ -14,17 +16,24 @@ public class GamePanel extends JPanel implements Runnable{
 	final int maxScreenRow=12;
 	final int screenWidth =tileSize*maxScreenCol;
 	final int screenHight = tileSize*maxScreenRow;
-	
-	
+	//fps
+	int FPS =60;
+	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
 	
+	
+	//set players default postion
+	int playerX =100;
+	int playerY= 100;
+	int playerSpeed = 4;
 	//constructor run when created it 
 	public GamePanel (){
 		this.setPreferredSize(new Dimension(screenWidth,screenHight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);//if set it true all the drawing from this component will be done in an offscreen painting buffer
 		                             //basically it improve rendering performance
-		
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 	
 	public void startGameThread() {
@@ -33,7 +42,96 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void run() {
-		
+		double drawInterval = 1000000000/FPS;
+		double delta =0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		int drawCount = 0;
+		long timer =0;
+		while (gameThread !=null) {
+			currentTime = System.nanoTime();
+			delta +=(currentTime - lastTime)/drawInterval;
+			timer+=(currentTime - lastTime);
+			lastTime = currentTime;
+			if(delta >=1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}
+			if (timer >= 1000000000) {
+				System.out.println("FPS:"+drawCount);
+				drawCount =0;
+				timer=0;
+			}
+			}
+		}
+	
+//	
+//	public void run() {
+//		
+//		//sleep method for seting fps
+//		double drawInterval = 1000000000/FPS;//1 seconds /60 mean draw screen 60 times per second
+//		
+//		double nextDrawTime = System.nanoTime() + drawInterval; //nano give systems current time then we add 1/60 seconds mean after that next pant will happens
+//		
+//		while(gameThread!=null) {
+//			//System.out.println("hi");
+//			//long currentTime = System.nanoTime();//to see current time 1 million nanosencds = 1 seconds
+//			
+//			
+//			//upadate : logic for updating the position of character
+//			update();
+//			
+//			//draw: draw the screen with update information
+//			repaint();
+//			// so if fps is 60 the program  does this (upadate and draw) 60 times per seconds 
+//			
+//			
+//			try {
+//				double remainingTime = nextDrawTime - System.nanoTime();// as we know how much update and repaint is fast so there will be remaining time after these methods
+//				remainingTime = remainingTime/1000000;//sleep only accept in milli seconds and we had remaining cal in nano so had to convert
+//				
+//				if(remainingTime <0) {
+//					remainingTime = 0;//if the update and rempant method took more time then astimate the sleep dont have to run thats why
+//				}
+//				Thread.sleep((long) remainingTime);
+//			
+//				nextDrawTime += drawInterval;
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}//let sleep for remaining time want to write Thread.sleep(remainingTime) but java rull did try catch 
+//			
+//		}
+//	}
+	public void update() {
+		if(keyH.upPressed ==true) {
+			playerY -= playerSpeed;
+		}
+		else if (keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}else if(keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}else if (keyH.rightPressed==true) {
+			playerX += playerSpeed;
+		}
+	}
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+//		1. public void paintComponent(Graphics g)
+//		This is a built-in method from the JPanel class that you are overriding.
+//
+//		The Graphics g parameter: Think of g as your Paintbrush. It is an object that has "powers" like g.drawRect(), g.drawImage(), or g.setColor(). Without this g, you can't see anything on the screen.
+//
+//		Automatic Call: You don't usually call this method yourself. Java calls it whenever the window needs to be refreshed (like when it first opens or when you tell it to "repaint").
+//	
+		Graphics2D g2 = (Graphics2D)g;
+		//graphics2d class extends the graphics class to provide more sophisticated control over geometry , coordinate transformation , color management, and text layout
+        g2.setColor(Color.white);	
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        g2.dispose();
+	
 	}
 	
 	
--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//it for when we click on x to close window
 		window.setResizable(false);//so that window does not resize itself
 		window.setTitle("JavaGame");
-		GamePanel gamePanel = new GamePanel();
+		GamePanel gamePanel = new GamePanel();//added to window just like jfram but it has more extra personal code
 		window.add(gamePanel);
 		window.pack();
 		window.setLocationRelativeTo(null);//not specify the location because it will displayed at the center 
@@ -16,7 +16,7 @@ public class Main {
 		
 		//in simple program the program gets process when we  do something like pressing button and then its stop and waits for another event
 		//but in 2d game program keeps runing
-		
+		gamePanel.startGameThread();
 	}
 
 }
